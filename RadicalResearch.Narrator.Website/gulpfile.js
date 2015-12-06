a80d7af8196e0e2f6282506b3b18c5ad4bb8d608
Sam Greenhalgh <github.com@zapthedingbat.com>
2015-12-06 20:48:24 +0000

    karma testing framework

--- RadicalResearch.Narrator.Website/gulpfile.js
+++ RadicalResearch.Narrator.Website/gulpfile.js
@@ -1,51 +1,23 @@
-/// <binding Clean='clean' />
+/* global __dirname */
 "use strict";
 
 var gulp = require("gulp"),
   shell = require("gulp-shell"),
   rimraf = require("rimraf"),
-  concat = require("gulp-concat"),
-  cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify"),
   project = require("./project.json");
 
-var paths = {
-  webroot: "./wwwroot/"
-};
+var Server = require('karma').Server;
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+var paths = {};
+paths.webroot = "./wwwroot/";
+paths.testConfig = "/Test/config";
 
-gulp.task('watch', shell.task(['dnx-watch web']));
+gulp.task('dnx-watch', shell.task(['dnx-watch web']));
+gulp.task('dnx', shell.task(['dnx web']));
 
-gulp.task("clean:js", function(cb) {
-  rimraf(paths.concatJsDest, cb);
+gulp.task("test", function(done){
+   new Server({
+    configFile: __dirname + paths.testConfig + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
-
-gulp.task("clean:css", function(cb) {
-  rimraf(paths.concatCssDest, cb);
-});
-
-gulp.task("clean", ["clean:js", "clean:css"]);
-
-gulp.task("min:js", function() {
-  gulp.src([paths.js, "!" + paths.minJs], {
-      base: "."
-    })
-    .pipe(concat(paths.concatJsDest))
-    .pipe(uglify())
-    .pipe(gulp.dest("."));
-});
-
-gulp.task("min:css", function() {
-  gulp.src([paths.css, "!" + paths.minCss])
-    .pipe(concat(paths.concatCssDest))
-    .pipe(cssmin())
-    .pipe(gulp.dest("."));
-});
-
-gulp.task("min", ["min:js", "min:css"]);
---
+++ RadicalResearch.Narrator.Website/test/test1.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Test 1', function(){
+	it('should pass', function(){
+		expect(true).to.be.true;
+	})
+});
